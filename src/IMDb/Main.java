783bf9c0cbadc68bdc8b1d894650ebdfe0bb6c7b
bruno7ferreira <b.rogerio2007@gmail.com>
2023-03-29 19:24:13 -0400

    atualizando nome do projeto

---
+++ src/IMDb/Main.java
@@ -0,0 +1,44 @@
+package IMDb;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
+
+import static java.net.http.HttpResponse.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Olá mundo!");
+        // fazer uma conexao http e pegar os dados do imdb
+        String url = "https://imdb-api.com/en/API/Top250TVs/k_tmhk2tbz";
+        URI endereco = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println();
+
+        // pegar somente dos dados que interessam(titulo,imagem,nota)
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        System.out.println(listaDeFilmes.size());
+        System.out.println(listaDeFilmes.get(0));
+
+
+        // exibir e manipular os dados
+
+        for (Map<String, String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+
+    }
+
+}
